48ec0110e7ff8eed3fb3b69834c2028ad863187f
barbil <bartol.bilic@fer.hr>
2019-10-17 21:41:11 +0200

    Added ServerListener interface

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/Connector.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/Connector.java
@@ -13,20 +13,39 @@ public class Connector {
      * This variable represents ipv4 address of your PC.
      * Change this variable depending on the ipv4 address of your PC.
      */
-    private static final String ipv4 = "ENTER_IPV4";
+    private static final String ipv4 = "192.168.1.109";
 
     /**
      * This variable represents url address of server.
      */
     private static final String url = "http://" + ipv4 + ":5000/";
 
+    /**
+     * This is interface which every Activity that wants to call
+     * methods provided in the class Connector must implement.
+     */
+    public interface ServerListener {
+        /**
+         * This method is used to make any action based on the <code>ServerStatus</code>
+         * returned by the Connector's <code>logIn</code> method.
+         * @param status status of server returned by Connector's <code>logIn</code> method.
+         */
+        void onLogInResponse(ServerStatus status);
+
+        /**
+         * This method is used to make any action based on the <code>ServerStatus</code>
+         * returned by the Connector's <code>register</code> method.
+         * @param status status of server returned by Connector's <code>register</code> method.
+         */
+        void onRegisterResponse(ServerStatus status);
+    }
+
     public static void logIn(final String userName, final String password, final Context context) {
 
     }
 
     public static void register(final String userName, final String password, final String firstName, final String lastName,
-                                 final byte[] picture, final String phone, final String email, final Context context) {
+                                final byte[] picture, final String phone, final String email, final Context context) {
 
     }
-
 }
