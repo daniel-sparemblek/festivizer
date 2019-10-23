f99526f5efcb29f30624f227fead246c154b57cb
Daniel <ds50919@fer.hr>
2019-10-23 23:56:43 +0200

    Removed unused imports, added multiple DB tables (user, specialization and event)

--- database_setup.py
+++ database_setup.py
@@ -1,23 +1,43 @@
-import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    
-    username = Column(String(250), primary_key=True)
-    password = Column(String(250), nullable=False)
-    firstname = Column(String(250), nullable=False)
-    lastname = Column(String(250), nullable=False)
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), primary_key=True)
+    password = Column(String(50), nullable=False)
+    firstname = Column(String(50), nullable=False)
+    lastname = Column(String(50), nullable=False)
     picture = Column(String(250), nullable=False)
-    phone = Column(String(250), unique=True)
-    email = Column(String(250), unique=True)
-    permission = Column(String(250))
+    phone = Column(String(50), unique=True)
+    email = Column(String(50), unique=True)
+    role = Column(String(50), nullable=False)
+    isPending = Column(Boolean)
+
+class Specialization(Base):
+    __tablename__ = 'specialization'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), unique=True)
+    
+class Event(Base)
+    __tablename__ = 'event'
+    
+    festival_id = Column(Integer, primary_key=True)
+    organizer_id = Column(Integer, primary_key=True)
+    name = Column(String(50), primary_key=True)
+    desc = Column(String(250))
+    location = Column(Integer, primary_key=True)
+    start_time = Column(DateTime, primary_key=True)
+    end_time = Column(DateTime)
+
+
 
 engine = create_engine('sqlite:///organizacijafestivala.db')
 Base.metadata.create_all(engine)
+
 
