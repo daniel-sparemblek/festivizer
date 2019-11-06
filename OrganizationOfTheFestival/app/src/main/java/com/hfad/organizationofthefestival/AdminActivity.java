87aea856e7bcedfbaa859b970da21e3218cf0e21
0036508924 <luka.lendel@fer.hr>
2019-11-06 15:45:14 +0100

    Enabled AdminActivity testing

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/AdminActivity.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/AdminActivity.java
@@ -2,35 +2,37 @@ package com.hfad.organizationofthefestival;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-<<<<<<< HEAD
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-=======
 import android.view.View;
 import java.util.ArrayList;
->>>>>>> 345c67953d2e70bf2d63fb86d3f58c4023727bb0
+
 
 public class AdminActivity extends AppCompatActivity implements AdminConnector.AdminListener {
+
+    private ArrayList<String> data = new ArrayList<>();
+    private ListView approvalList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
 
+        approvalList = findViewById(R.id.approvalList);
 
-        ListView approvalList = findViewById(R.id.approvalList);
+        AdminConnector.getPendingLeaders("Bartz", "134", this);
 
-        ArrayAdapter<String> approvalListAdapter = new ArrayAdapter<>(this, R.layout.admin_row_layout, );
 
-        approvalList.setAdapter();
     }
 
     public void onClickGetPendingLeaders(View view) {
-
+        AdminConnector.getPendingLeaders("Bartz", "134", this);
     }
 
     @Override
     public void onAdminResponse(ArrayList<String> pendingLeaders) {
-
+        data = pendingLeaders;
+        ArrayAdapter<String> approvalListAdapter = new ArrayAdapter<>(this, R.layout.skroznovi, data);
+        approvalList.setAdapter(approvalListAdapter);
     }
 }
