d3bae715f99160679d87074dfee2c32af7a9df8b
barbil <bartol.bilic@fer.hr>
2019-11-06 14:35:36 +0100

    Added methods for Administrator

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/AdminActivity.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/AdminActivity.java
@@ -2,12 +2,23 @@ package com.hfad.organizationofthefestival;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import java.util.ArrayList;
 
-public class AdminActivity extends AppCompatActivity {
+public class AdminActivity extends AppCompatActivity implements AdminConnector.AdminListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+    }
+
+    public void onClickGetPendingLeaders(View view) {
+
+    }
+
+    @Override
+    public void onAdminResponse(ArrayList<String> pendingLeaders) {
+
     }
 }
