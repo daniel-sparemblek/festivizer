cdc133ae035ac90df52900668cdaead072876eb0
barbil <bartol.bilic@fer.hr>
2019-11-09 00:16:56 +0100

    Added necessary imports

--- database_setup.py
+++ database_setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, ForeignKey, UniqueConstraint
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, ForeignKey, UniqueConstraint, ForeignKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 
