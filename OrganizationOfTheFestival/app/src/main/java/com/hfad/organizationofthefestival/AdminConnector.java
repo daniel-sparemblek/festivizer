42cd323013a486c68675741c8d610f7bce56a101
Daniel R. Sparemblek <ds50919@fer.hr>
2019-11-10 19:07:18 +0000

    Added sendDecision method

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/AdminConnector.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/AdminConnector.java
@@ -79,8 +79,41 @@ public class AdminConnector extends Connector {
     }
 
 
-    public static void sendAdminDecision(final String adminUsername, final String adminPassword, final String leaderUsername, final Decision decision, final Context context) {
-        //to do
+    public static void sendDecision(final String username, final String password, final String leaderUsername,
+                                    final Decision decision, final Context context) {
+        RequestQueue queue = Volley.newRequestQueue(context);
+        String adminUrl = url + "admin/send_decision";
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, adminUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        ((AdminListener) context).onSendDecisionResponse(getStatus(response));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        ((AdminListener) context).onSendDecisionResponse(getStatus("server_down"));
+                    }
+                }
+        ) {
+            @Override
+            public String getBodyContentType() {
+                return "application/x-www-form-urlencoded; charset=UTF-8";
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("user_identifier", username);
+                params.put("password", password);
+                params.put("leader_username", leaderUsername);
+                params.put("decision", convertDecisionToString(decision));
+                return params;
+            }
+        };
+        queue.add(postRequest);
     }
 
 }
