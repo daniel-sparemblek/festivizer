dbd5132b63dd624a28f63165f9858652931a6447
0036508924 <luka.lendel@fer.hr>
2019-11-11 12:48:31 +0100

    Addeed Festival class, OrganizerAdapter and OrganizerRow ; Modified OrganizerActivity and layout

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/OrganizerActivity.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/OrganizerActivity.java
@@ -1,13 +1,49 @@
 package com.hfad.organizationofthefestival;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.hfad.organizationofthefestival.adapters.OrganizerAdapter;
+
+import java.util.ArrayList;
 
 public class OrganizerActivity extends AppCompatActivity {
+
+    private ArrayList<Festival> festivals;
+    private ListView listView;
+    private static OrganizerAdapter adapter;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organizer);
+
+        button = findViewById(R.id.btnApply);
+        listView = findViewById(R.id.festivalList);
+
+        festivals = new ArrayList<>();
+        Festival festival = new Festival(444, 777, "jebanje", "bas jako jebanje", 7777777, true);
+        festivals.add(festival);
+        // get festivals from database
+
+        adapter = new OrganizerAdapter(festivals, getApplicationContext());
+
+        listView.setAdapter((adapter));
+    }
+
+    public void onClickApply(View view) {
+        Context context;
+        Toast toast;
+        context = getApplicationContext();
+        CharSequence message = "Bravo pedercino";
+        int duration = Toast.LENGTH_SHORT;
+        toast = Toast.makeText(context, message, duration);
+        toast.show();
     }
 }
---
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/adapters/OrganizerAdapter.java
@@ -0,0 +1,57 @@
+package com.hfad.organizationofthefestival.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.hfad.organizationofthefestival.Festival;
+import com.hfad.organizationofthefestival.R;
+
+import java.util.ArrayList;
+
+public class OrganizerAdapter extends ArrayAdapter<Festival> {
+
+    private ArrayList<Festival> festivals;
+    Context context;
+
+    private static class ViewHolder {
+        TextView festivalName;
+        Button appButton;
+    }
+
+
+    public OrganizerAdapter(ArrayList<Festival> festivals, Context context) {
+        super(context, R.layout.organizer_row_layout, festivals);
+        this.festivals = festivals;
+        this.context = context;
+    }
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Festival festival = getItem(position);
+        final View result;
+        ViewHolder viewHolder;
+
+        if(convertView == null) {
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.organizer_row_layout, parent, false);
+
+            viewHolder.festivalName = convertView.findViewById(R.id.festivalName);
+            viewHolder.appButton = convertView.findViewById(R.id.btnApply);
+
+            result = convertView;
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+
+        viewHolder.festivalName.setText(festival.getName());
+        return convertView;
+    }
+}
