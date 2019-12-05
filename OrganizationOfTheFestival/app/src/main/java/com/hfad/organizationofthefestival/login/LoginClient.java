711ab3c18490e34b3458ac3c6741d4ad80d2b960
0036508924 <luka.lendel@fer.hr>
2019-12-05 13:42:31 +0100

    Added a new method to Login API and started the implementation

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/login/LoginClient.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/login/LoginClient.java
@@ -1,13 +1,19 @@
 package com.hfad.organizationofthefestival.login;
 
+import com.hfad.organizationofthefestival.User;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface LoginClient {
 
     @POST("login")
     Call<LoginResponse> login(@Body Login login);
 
-    // TODO add more methods
+    @GET("{username}")
+    Call<User> getUser(@Path("username") String username, @Header("Authorization") String authorization);
 }
