05e601fccbf7ad16b4069636590e8fec99d2013f
0036508924 <luka.lendel@fer.hr>
2019-12-06 23:21:39 +0100

    Added Leader API and Festivals model

---
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/leader/LeaderClient.java
@@ -0,0 +1,13 @@
+package com.hfad.organizationofthefestival.leader;
+
+import com.hfad.organizationofthefestival.festival.Festivals;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+
+public interface LeaderClient {
+    @GET("festivals")
+    Call<Festivals> getFestivals(@Header("Authorization") String authorization);
+}
