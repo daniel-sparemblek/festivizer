90b960bb723378d852150e11ca8502e56fc536b0
Karlo Ježić <karlo.jezic@fer.hr>
2019-12-06 18:26:46 +0100

    Minor changes

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/leader/Leader.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/leader/Leader.java
@@ -1,9 +1,9 @@
 package com.hfad.organizationofthefestival.leader;
 
-import com.hfad.organizationofthefestival.Role;
 import com.hfad.organizationofthefestival.User;
 
 public class Leader extends User {
+
     public Leader(String username, String password, String firstName, String lastName, String picture, String phone, String email, int permission) {
         super(username, password, firstName, lastName, picture, phone, email, permission);
     }
