ecee018524632174b3a7edecc55460b2189a8812
Karlo Ježić <karlo.jezic@fer.hr>
2019-12-06 20:41:51 +0100

    Worker can now connect to server

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/WorkerActivity.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/WorkerActivity.java
@@ -1,5 +1,6 @@
 package com.hfad.organizationofthefestival.worker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,11 +8,27 @@ import com.hfad.organizationofthefestival.R;
 
 public class WorkerActivity extends AppCompatActivity {
 
+    private WorkerController workerController;
+    private String accessToken;
+    private String refreshToken;
+    private String username;
+    private Worker worker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_worker);
 
+        Intent intent = getIntent();
+        accessToken = intent.getStringExtra("accessToken");
+        refreshToken = intent.getStringExtra("refreshToken");
+        username = intent.getStringExtra("username");
 
+
+        workerController = new WorkerController(this);
+
+        workerController.getWorker(accessToken, username, worker);
+
+        //TODO all info can be filled up
     }
 }
--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/WorkerController.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/WorkerController.java
@@ -1,6 +1,12 @@
 package com.hfad.organizationofthefestival.worker;
 
+import android.widget.Toast;
+
 import com.hfad.organizationofthefestival.signup.RegistrationResponse;
+
+import org.json.JSONObject;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,17 +30,54 @@ public class WorkerController {
 
     private WorkerClient workerClient = retrofit.create(WorkerClient.class);
 
-    public void getWorker(String accessToken, String username){
+    public void getWorker(final String accessToken, String username, final Worker worker) {
         Call<Worker> call = workerClient.getWorker(username, accessToken);
 
         call.enqueue(new Callback<Worker>() {
             @Override
             public void onResponse(Call<Worker> call, Response<Worker> response) {
+                if (response.isSuccessful()) {
+                    getSpecializations(accessToken, worker);
+                } else {
+                    try {
+                        JSONObject errorObject = new JSONObject(response.errorBody().string());
+                        Toast.makeText(workerActivity, errorObject.getString("message"), Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(workerActivity, "Something went wrong. Please try again!", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
             public void onFailure(Call<Worker> call, Throwable t) {
+                Toast.makeText(workerActivity, "Server-side or internet error on fetching user data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
+    public void getSpecializations(String accessToken, final Worker worker) {
+        Call<List<Specialization>> call = workerClient.getSpecializations(accessToken);
+
+        call.enqueue(new Callback<List<Specialization>>() {
+            @Override
+            public void onResponse(Call<List<Specialization>> call, Response<List<Specialization>> response) {
+                if (response.isSuccessful()) {
+                    worker.setSpecializations(response.body());
+                } else {
+                    try {
+                        JSONObject errorObject = new JSONObject(response.errorBody().string());
+                        Toast.makeText(workerActivity, errorObject.getString("message"), Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(workerActivity, "Something went wrong. Please try again!", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Specialization>> call, Throwable t) {
+                Toast.makeText(workerActivity, "Server-side or internet error on fetching user data", Toast.LENGTH_SHORT).show();
             }
         });
     }
