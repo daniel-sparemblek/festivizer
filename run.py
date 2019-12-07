d3e9818574253f730f897fc5c09d8b0fac7dca99
barbil <bartol.bilic@fer.hr>
2019-12-07 02:40:35 +0100

    Added new endpoints

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager
+from flask_jwt_extended import JWTManager, jwt_refresh_token_required, get_jwt_identity
 from flask_marshmallow import Marshmallow
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -13,6 +14,7 @@ app.config['SECRET_KEY'] = 'b\'\xbe\xadI\x13\xaf\x959f\x18=\x8d\x06Q\x9a\xd6y\x9
 app.config['JWT_SECRET_KEY'] = 'b\'\x856\xbb\xdf\xf49S\xfep\x1eJEc>\xf1\xec(\x08n#6\x01*\xa6\''
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
@@ -30,14 +32,17 @@ def check_if_token_in_blacklist(decrypted_token):
 @app.before_first_request
 def create_tables():
     db.create_all()
+    db.query_expression
 
 
 api.add_resource(resources.UserRegistration, '/registration')
 api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.User, '/user/<string:username>')
+api.add_resource(resources.Users, '/users')
 api.add_resource(resources.UserLogoutAccess, '/logout/access')
 api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
 api.add_resource(resources.TokenRefresh, '/token/refresh')
-api.add_resource(resources.AllUsers, '/users')
-api.add_resource(resources.AllLeaders, '/leaders')
 api.add_resource(resources.SecretResource, '/secret')
 api.add_resource(resources.Test, '/test')
+
+
