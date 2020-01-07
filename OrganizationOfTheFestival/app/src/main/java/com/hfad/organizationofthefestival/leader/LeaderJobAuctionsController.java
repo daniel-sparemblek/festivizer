4259e4f541104a7312bb2c8c320da715f9cee666
Karlo Ježić <karlo.jezic@fer.hr>
2020-01-07 17:40:32 +0100

    new class LeaderJobAuctionsController

---
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/leader/LeaderJobAuctionsController.java
@@ -0,0 +1,27 @@
+package com.hfad.organizationofthefestival.leader;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class LeaderJobAuctionsController {
+    private LeaderJobAuctionsActivity activity;
+    private String accessToken;
+    private String refreshToken;
+    private LeaderClient api;
+
+    public LeaderJobAuctionsController(LeaderJobAuctionsActivity activity, String accessToken, String refreshToken) {
+        api = new Retrofit.Builder()
+                .baseUrl("https://kaogrupa.pythonanywhere.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(LeaderClient.class);
+
+        this.activity = activity;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public void getJobAuctions(){
+        api.get
+    }
+}
