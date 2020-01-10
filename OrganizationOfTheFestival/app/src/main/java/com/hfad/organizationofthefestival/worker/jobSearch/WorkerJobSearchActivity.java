370ca776791fdb496a176ddcb018ac594136d397
Karlo Ježić <karlo.jezic@fer.hr>
2020-01-10 22:02:17 +0100

    worker job search controller done

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/jobSearch/WorkerJobSearchActivity.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/jobSearch/WorkerJobSearchActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -14,10 +15,17 @@ import com.hfad.organizationofthefestival.utility.Job;
 
 public class WorkerJobSearchActivity extends AppCompatActivity {
 
-    private EditText etComment;
-    private Button btnAddComment;
     private Job job;
     private int event;
+
+    private EditText etComment;
+    private Button btnAddComment;
+    private TextView tvJobName;
+    private TextView tvJobDescription;
+    private TextView tvEvent;
+    private TextView tvWorkerName;
+    private TextView tvStartTime;
+    private TextView tvFestivalName;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -36,9 +44,14 @@ public class WorkerJobSearchActivity extends AppCompatActivity {
     private void findViews(){
         etComment = findViewById(R.id.org_searchTxt);
         btnAddComment = findViewById(R.id.add_comment);
+        tvEvent = findViewById(R.id.org_event);
+        tvJobDescription = findViewById(R.id.jobDesc);
+        tvWorkerName = findViewById(R.id.org_workerName);
+        tvJobName = findViewById(R.id.jobName);
+        tvStartTime = findViewById(R.id.org_startTime);
+        tvFestivalName = findViewById(R.id.org_fest);
     }
 
-    private void fillInActivity(){
-
+    public void fillInActivity(String festivalName) {
     }
 }
--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/jobSearch/WorkerJobSearchController.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/jobSearch/WorkerJobSearchController.java
@@ -1,4 +1,60 @@
 package com.hfad.organizationofthefestival.worker.jobSearch;
 
+import android.widget.Toast;
+
+import com.hfad.organizationofthefestival.utility.JobApply;
+import com.hfad.organizationofthefestival.worker.WorkerClient;
+
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class WorkerJobSearchController {
+
+    private WorkerJobSearchActivity activity;
+    private String accessToken;
+    private String refershToken;
+    private WorkerClient api;
+
+    public WorkerJobSearchController(WorkerJobSearchActivity activity, String accessToken, String refershToken) {
+        api = new Retrofit.Builder()
+                .baseUrl("https://kaogrupa.pythonanywhere.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(WorkerClient.class);
+
+        this.activity = activity;
+        this.accessToken = accessToken;
+        this.refershToken = refershToken;
+    }
+
+    private void getFestivalName(String jobId){
+        Call<JobApply> call = api.getJob(jobId, "Bearer " + accessToken);
+
+        call.enqueue(new Callback<JobApply>() {
+            @Override
+            public void onResponse(Call<JobApply> call, Response<JobApply> response) {
+                if (response.isSuccessful()){
+                    activity.fillInActivity(response.body().getEvent().getFestival().getName());
+                } else {
+                    try {
+                        JSONObject errorObject = new JSONObject(response.errorBody().string());
+                        Toast.makeText(activity, errorObject.getString("msg"), Toast.LENGTH_SHORT).show();
+                    } catch (Exception e) {
+                        Toast.makeText(activity, "Something went wrong. Please try again!", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<JobApply> call, Throwable t) {
+                Toast.makeText(activity, "Server-side or internet error on fetching user data", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
