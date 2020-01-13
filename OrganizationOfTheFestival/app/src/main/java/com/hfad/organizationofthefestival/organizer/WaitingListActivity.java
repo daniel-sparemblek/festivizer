db8431c4ffad5d7184a74d8514fb91405a0f9d19
0036508924 <luka.lendel@fer.hr>
2020-01-13 16:53:12 +0100

    Waiting list toolbar fixed

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/organizer/WaitingListActivity.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/organizer/WaitingListActivity.java
@@ -3,6 +3,7 @@ package com.hfad.organizationofthefestival.organizer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.hfad.organizationofthefestival.R;
 
@@ -16,6 +17,10 @@ public class WaitingListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.organizer_screen_waiting_list);
+
+        Toolbar toolbar = findViewById(R.id.organizer_toolbar);
+        toolbar.setTitle("Waiting list");
+        setSupportActionBar(toolbar);
 
         Intent intent = getIntent();
         accessToken = intent.getStringExtra("accessToken");
