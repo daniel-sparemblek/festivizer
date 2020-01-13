f94b467d4875ce8fbf893b91813f72148b4ccbe9
Petra <petra.fribert@fer.hr>
2020-01-14 00:58:44 +0100

    will this merge end

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/organizer/ViewEventActivity.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/organizer/ViewEventActivity.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ViewEventActivity extends AppCompatActivity {
 
