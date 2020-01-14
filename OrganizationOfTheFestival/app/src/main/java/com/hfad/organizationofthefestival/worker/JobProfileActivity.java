ac5fa49869dad1e54af1e07b3f02a97a0c6e1715
Karlo Ježić <karlo.jezic@fer.hr>
2020-01-14 02:15:04 +0100

    JobProfileActivity design changes

--- OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/JobProfileActivity.java
+++ OrganizationOfTheFestival/app/src/main/java/com/hfad/organizationofthefestival/worker/JobProfileActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.Html;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -12,6 +13,10 @@ import android.widget.TextView;
 import com.hfad.organizationofthefestival.R;
 import com.hfad.organizationofthefestival.utility.JobApply;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -46,6 +51,10 @@ public class JobProfileActivity extends AppCompatActivity {
         username = intent.getStringExtra("username");
         jobId = intent.getIntExtra("job_id", 0);
 
+        Toolbar toolbar = findViewById(R.id.worker_toolbar);
+        toolbar.setTitle("Job profile");
+        setSupportActionBar(toolbar);
+
         tvName = findViewById(R.id.jobName);
         tvDescription = findViewById(R.id.jobDesc);
         tvStartTime = findViewById(R.id.startTime);
@@ -69,7 +78,9 @@ public class JobProfileActivity extends AppCompatActivity {
         tvName.setText(body.getName());
         tvFestivalName.setText(body.getEvent().getFestival().getName());
         tvEventName.setText(body.getEvent().getName());
-        tvStartTime.setText(body.getStartTime());
+        tvStartTime.setText(parseDateTime(body.getStartTime())
+                .truncatedTo(ChronoUnit.MINUTES)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")));
         tvDescription.setText(body.getDescription());
 
         ArrayAdapter<String> specializationArrayAdapter = new ArrayAdapter<>(this,
@@ -84,5 +95,16 @@ public class JobProfileActivity extends AppCompatActivity {
                 .map(Specialization::getName)
                 .collect(Collectors.toList());
         return stringList;
+    }
+
+    private ZonedDateTime parseDateTime(String dateTime) {
+        int year = Integer.parseInt(dateTime.substring(0, 4));
+        int month = Integer.parseInt(dateTime.substring(5, 7));
+        int day = Integer.parseInt(dateTime.substring(8, 10));
+        int hour = Integer.parseInt(dateTime.substring(11, 13));
+        int minute = Integer.parseInt(dateTime.substring(14, 16));
+        int second = Integer.parseInt(dateTime.substring(17, 19));
+
+        return ZonedDateTime.of(year, month, day, hour, minute, second, 0, ZoneId.systemDefault());
     }
 }
